8d39ac77fe17d7ecf75f95243e8f99cd91f432a3
Nick Ng <nick-ng@users.noreply.github.com>
2024-03-16 15:23:46 +1300

    feat: add all cards from the recommended starting set

--- src/lib/components/card-contents.svelte
+++ src/lib/components/card-contents.svelte
@@ -14,25 +14,45 @@
 		: "";
 </script>
 
-<CardFrame fullImageUrl={card?.fullImage && card?.imageUrl} class="relative">
+<CardFrame
+	fullImageUrl={card?.fullImage && card?.imageUrl}
+	class="relative leading-tight"
+>
 	<h4 class="mb-1 leading-none">{card.displayNames[0]}</h4>
 	{#if isFullImage || !card.imageUrl}
 		<div class="flex-grow" />
 	{:else}
-		<div class="mx-1 mb-1 flex-shrink basis-16 border" style={imageStyle} />
+		<div class="mx-1 mb-0 flex-shrink basis-16 border" style={imageStyle} />
 	{/if}
 	{#each card.effects as effect}
-		{#if effect.type === "action"}
+		{#if effect.type === "card"}
+			<div class="text-center text-sm">
+				+{effect.value}
+				{effect.value === 1 ? "Card" : "Cards"}
+			</div>
+		{:else if effect.type === "action"}
 			<div class="text-center text-sm">
 				+{effect.value}
 				{effect.value === 1 ? "Action" : "Actions"}
 			</div>
+		{:else if effect.type === "buy"}
+			<div class="text-center text-sm">
+				+{effect.value}
+				{effect.value === 1 ? "Buy" : "Buyss"}
+			</div>
+		{:else if effect.type === "coin"}
+			<div class="text-center text-sm">
+				+<Coin />
+				{effect.value}
+			</div>
+		{:else if effect.type === "line"}
+			<hr />
 		{:else}
-			<div class="text-left text-xs">{effect.description}</div>
+			<div class="text-center text-xs">{effect.description}</div>
 		{/if}
 	{/each}
 	{#if card.coins}
-		<div><Coin /> +{card.coins}</div>
+		<div>+<Coin /> {card.coins}</div>
 	{/if}
 	{#if card.victoryPoints}
 		<div><VictoryPoint /> {card.victoryPoints}</div>
@@ -41,12 +61,12 @@
 		<div class="flex-grow" />
 	{/if}
 	<div
-		class="mt-1 flex flex-row justify-between border-t pt-0.5 text-left text-sm"
+		class="mt-1 flex flex-row justify-between border-t pt-0.5 text-left text-xs"
 	>
 		<div>
 			<Coin />
 			{card.cost}
 		</div>
-		<div class="capitalize">{card.types.join(" - ")}</div>
+		<div class="capitalize">{card.types.join(", ")}</div>
 	</div>
 </CardFrame>
--- src/lib/game/base-game-card-list.ts
+++ src/lib/game/base-game-card-list.ts
@@ -1,5 +1,7 @@
 import type { Card } from "$lib/schemas/types";
 
+import { coinEmoji } from "../emojis";
+
 export const BASE_GAME_CARDS: { [key: string]: Card } = {
 	cellar: {
 		types: ["action"],
@@ -18,7 +20,7 @@ export const BASE_GAME_CARDS: { [key: string]: Card } = {
 	cellary: {
 		types: ["action"],
 		name: "cellary",
-		displayNames: ["Cellary", "Cellars"],
+		displayNames: ["Cellary", "Cellaries"],
 		imageUrl: "cellar.png",
 		effects: [
 			{ type: "action", value: 1 },
@@ -29,5 +31,121 @@ export const BASE_GAME_CARDS: { [key: string]: Card } = {
 			},
 		],
 		cost: 2,
+	},
+	moat: {
+		types: ["action", "reaction"],
+		name: "moat",
+		displayNames: ["Moat", "Moats"],
+		imageUrl: "favicon.png",
+		effects: [
+			{ type: "action", value: 1 },
+			{ type: "line" },
+			{
+				type: "moat-0",
+				description:
+					"When another player plays an Attack card, you may first reveal this from your hand, to be unaffected by it.",
+			},
+		],
+		cost: 2,
+	},
+	village: {
+		types: ["action"],
+		name: "village",
+		displayNames: ["Village", "Villages"],
+		imageUrl: "favicon.png",
+		effects: [
+			{ type: "card", value: 1 },
+			{ type: "action", value: 2 },
+		],
+		cost: 3,
+	},
+	merchant: {
+		types: ["action"],
+		name: "merchant",
+		displayNames: ["Merchant", "Merchants"],
+		imageUrl: "favicon.png",
+		effects: [
+			{ type: "card", value: 1 },
+			{ type: "action", value: 1 },
+			{
+				type: "merchant-0",
+				description: `The first time you play a Silver this turn, +${coinEmoji} 1`,
+			},
+		],
+		cost: 3,
+	},
+	workshop: {
+		types: ["action"],
+		name: "workshop",
+		displayNames: ["Workshop", "Workshops"],
+		imageUrl: "favicon.png",
+		effects: [
+			{
+				type: "workshop-0",
+				description: `Gain a card costing up to ${coinEmoji} 4`,
+			},
+		],
+		cost: 3,
+	},
+	smithy: {
+		types: ["action"],
+		name: "smithy",
+		displayNames: ["Smithy", "Smithies"],
+		imageUrl: "favicon.png",
+		effects: [{ type: "card", value: 3 }],
+		cost: 4,
+	},
+	remodel: {
+		types: ["action"],
+		name: "remodel",
+		displayNames: ["Remodel", "Remodels"],
+		imageUrl: "favicon.png",
+		effects: [
+			{
+				type: "remodel-0",
+				description: `Trash a card from your hand. Gain a card costing up to ${coinEmoji} 2 more than it.`,
+			},
+		],
+		cost: 4,
+	},
+	militia: {
+		types: ["action", "attack"],
+		name: "militia",
+		displayNames: ["Militia", "Militias"],
+		imageUrl: "favicon.png",
+		effects: [
+			{ type: "coin", value: 2 },
+			{
+				type: "militia-0",
+				description: `Each other player discards down to 3 cards in hand.`,
+			},
+		],
+		cost: 4,
+	},
+	market: {
+		types: ["action", "attack"],
+		name: "market",
+		displayNames: ["Market", "Markets"],
+		imageUrl: "favicon.png",
+		effects: [
+			{ type: "card", value: 1 },
+			{ type: "action", value: 1 },
+			{ type: "buy", value: 1 },
+			{ type: "coin", value: 1 },
+		],
+		cost: 5,
+	},
+	mine: {
+		types: ["action"],
+		name: "mine",
+		displayNames: ["Mine", "Mines"],
+		imageUrl: "favicon.png",
+		effects: [
+			{
+				type: "mine-0",
+				description: `You may trash a Treasure from your hand. Gain a Treasure to your hand costing up to ${coinEmoji} 3 more than it.`,
+			},
+		],
+		cost: 4,
 	},
 };
