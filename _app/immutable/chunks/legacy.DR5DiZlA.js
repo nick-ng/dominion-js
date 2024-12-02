4684add113a8efbcb1e2921294e3b44c3cfebae8
nick-ng <nick-ng@users.noreply.github.com>
2024-12-02 08:24:04 +0000

    Mon Dec  2 08:24:04 UTC 2024 deploy front-end

---
+++ _app/immutable/chunks/legacy.DR5DiZlA.js
@@ -0,0 +1,1 @@
+import{m as a}from"./runtime.BxkViHBk.js";a();
---
+++ _app/immutable/nodes/2.D3LRdLpM.js
@@ -0,0 +1,1 @@
+import{a as t,t as n}from"../chunks/disclose-version.BRY8-qa2.js";import"../chunks/legacy.DR5DiZlA.js";const p="https://jurisdiction-server.pux.one";var e=n("<div><h1>Jurisdiction</h1> <p>Dominion</p></div>");function s(o){console.debug("PUBLIC_API_HOST",p);var i=e();t(o,i)}export{s as component};
